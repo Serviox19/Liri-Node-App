568b4c0f86dbb8a93faf85da02a2def6e9c2e7b9
Serviox19 <servio19@gmail.com>
2016-01-20 18:21:31 -0500

    fixed main file so that it can grap the users request and use the corresponding js file

--- liri.js
+++ liri.js
@@ -1,2 +1,16 @@
 var request = require('request');
+var fs = require('fs');
 
+switch(process.argv[2]){
+  case 'my-tweets':
+    var getTweets = require('./twitter.js');
+  break;
+  
+  case 'spotify-this-song':
+    var getSong = require('./spotify.js');
+  break;
+
+  case 'movie-this':
+    var getMovies = require('./movie.js');
+  break;  
+}
